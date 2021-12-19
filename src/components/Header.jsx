f7f8e718570c14da92457d169cc82fa5d7bd380d
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2021-12-19 23:35:45 +0900

    💄

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="h-10 flex justify-between px-4 border-b">
+      <h1 className="my-auto">design components</h1>
+      <button className="border">maru</button>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/tab/Tab1.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import Head from "next/head";
+
+const Tab1 = () => {
+  const tabArray = ["aaa", "bbb", "ccc", "ddd"];
+  const [tab, setTab] = useState(tabArray[0]);
+
+  const handleClickTab = (e) => {
+    console.log(e.target.innerHTML);
+    setTab(e.target.innerHTML);
+  };
+
+  return (
+    <div>
+      <div className="h-[200px] w-screen">
+        <div className="mx-auto w-2/3 ">
+          <div className="border-b">
+            {tabArray.map((tabName) => (
+              <button
+                className={
+                  tabName === tab
+                    ? "w-[70px] mr-1 text-lg hover:text-blue-300 transition text-blue-500  border-b-2 border-blue-500 "
+                    : "w-[70px] mr-1 text-lg  hover:text-blue-300 transition"
+                }
+                onClick={handleClickTab}
+              >
+                {tabName}
+              </button>
+            ))}
+          </div>
+          <div className={"aaa" === tab ? "" : "hidden"}>
+            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+            <br />
+            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+          </div>
+          <div className={"bbb" === tab ? "" : "hidden"}>
+            bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+            <br />
+            bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+          </div>
+          <div className={"ccc" === tab ? "" : "hidden"}>
+            cccccccccccccccccccccccccccccccccccccccccccccccccc
+            <br />
+            cccccccccccccccccccccccccccccccccccccccccccccccccc
+          </div>
+          <div className={"ddd" === tab ? "" : "hidden"}>
+            dddddddddddddddddddddddddddddddddddddddddddddddddd
+            <br />
+            dddddddddddddddddddddddddddddddddddddddddddddddddd
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tab1;
