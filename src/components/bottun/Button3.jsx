41fbba3db5fedbe35768acb63021a56e2d4aee15
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2021-12-24 23:44:34 +0900

    💄

--- src/components/bottun/Button3.jsx
+++ src/components/bottun/Button3.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Button2 = (props) => {
+const Button3 = (props) => {
   return (
     <button className="h-10 w-20  text-sm border text-white border-gray-500 bg-gray-500 transition hover:bg-white hover:text-gray-500 hover:transition active:opacity-60 active:border-blue-200 active:text-blue-200  ">
       {props.children}
@@ -8,4 +8,4 @@ const Button2 = (props) => {
   );
 };
 
-export default Button2;
+export default Button3;
--- src/pages/buttons.jsx
+++ src/pages/buttons.jsx
@@ -1,19 +1,24 @@
+import { comment } from "postcss";
 import React from "react";
 import Button1 from "../components/bottun/Button1";
 import Button2 from "../components/bottun/Button2";
+import Button3 from "../components/bottun/Button3";
 import Header from "../components/Header";
 
 const Buttons = () => {
+  const a = [<Button1 />, <Button2>ボタン</Button2>, <Button3>ボタン</Button3>];
+
   return (
     <div>
       <Header />
       <div className="m-4 flex gap-3 ">
-        <div className="flex justify-center w-40 h-40  border shadow-lg">
-          <Button1 />
-        </div>
-        <div className="flex justify-center w-40 h-40 items-center border shadow-lg">
-          <Button2>ボタン</Button2>
-        </div>
+        {a.map((button) => {
+          return (
+            <div className="flex justify-center w-40 h-40 items-center border shadow-lg">
+              {button}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
